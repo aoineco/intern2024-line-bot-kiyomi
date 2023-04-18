993a607ce89a6715d15ceefb4700ed5da6d7dd3d
gifteeintern4 <gifteeintern4@gifteeintern4noMacBook-Pro.local>
2023-04-18 11:12:48 +0900

    本番環境にデータを追加する処理を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+ChallengeTask.find_or_create_by(content:"普段言わない人に感謝の気持ちを伝えよう！家族、友人、同僚誰でも構いません")
+ChallengeTask.find_or_create_by(content:"コミュニティ内で話さない人に話かけてみよう！")
+ChallengeTask.find_or_create_by(content:"店員さんと世間話をしてみよう！")
+ChallengeTask.find_or_create_by(content:"困っている人を積極的に助けてみよう！５回できればクリア！")
+ChallengeTask.find_or_create_by(content:"いつもと違う人と食事をしてみよう！")
+ChallengeTask.find_or_create_by(content:"自分が知っているつもりでも考えてみると知らないことを１つ考えてみよう！そのことについて本、インターネットで調べてみよう！")
+ChallengeTask.find_or_create_by(content:"食わず嫌いになっている食べ物を食べてみよう！食べてみると意外と美味しいかも！？")
+ChallengeTask.find_or_create_by(content:"行ったことの無い近くのお店に立ち寄ってみよう！新たな出会いがあるかも！？")
+ChallengeTask.find_or_create_by(content:"いつもと違う道・交通手段で目的地へ行ってみよう！")
+ChallengeTask.find_or_create_by(content:"地図アプリを使わずに目的地へ行ってみよう！")
+ChallengeTask.find_or_create_by(content:"１日利き手じゃない方の手を使って活動してみよう！（歯磨き、ドライヤーなど）")
+ChallengeTask.find_or_create_by(content:"明日はいつもより早く起きてみよう！早起きは三文の徳！")
+ChallengeTask.find_or_create_by(content:"いつもと違う場所で作業してみよう！（カフェ、リビング、図書館など）")  
